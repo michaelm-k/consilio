4ab7db9a6dc0a0b3d2c5de48a78008819021b05b
Michael Marek <atlikeross@gmail.com>
2015-09-03 15:38:37 -0500

    Trying to get sio to work ffs

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var http = require('http');
+var sio = require('socket.io');
 var app = express();
 var port = process.env.PORT || 5000;
 var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+
+var io = sio.listen(server);
+
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 20); 
+});
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
@@ -65,9 +72,4 @@ var run_server = server.listen(port, function () {
   var port = run_server.address().port;
 
   console.log('Listening at http://%s:%s', host, port);
-});
-
-io.configure(function () { 
-    io.set("transports", ["xhr-polling"]); 
-    io.set("polling duration", 10); 
 });

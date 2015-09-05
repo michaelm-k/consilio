461e4bdce25e869a3255d9cb4857dd3c978ff1d2
Michael Marek <atlikeross@gmail.com>
2015-09-05 16:39:32 -0500

    Public rooms visible, updated in RT onnon-chat pages

--- app.js
+++ app.js
@@ -15,13 +15,17 @@ app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 app.use("/static", express.static(__dirname + '/static'));
 
-var rooms = {}; // empty object of all room numbers
+var room_numbers = {}; // empty object of all room numbers
 var numUsers=0;
 var sockets = [];
 var keys={};
+var public_rooms = {};
+public_rooms['lobby']='lobby';
+var numUsersInPublicRooms = {};
+numUsersInPublicRooms['lobby']=0;
 
-function findRoomByRoomNumber(room_number) {
-    if (rooms.hasOwnProperty(room_number)) {
+function isRoomNumberOccupied(room_number) {
+    if (room_numbers.hasOwnProperty(room_number)) {
         return true;
     }
     return false;
@@ -37,7 +41,7 @@ app.get('/', function (req, res) {
 
 app.get('/:room', function (req, res) {
     var room_number = req.params.room;
-    if (findRoomByRoomNumber(room_number)) {
+    if (isRoomNumberOccupied(room_number)) {
         res.render("chat"); 
     } else {
          res.render("rooms");
@@ -47,14 +51,15 @@ app.get('/:room', function (req, res) {
 app.post('/rooms/new', function (req, res) {
     var room_number = req.body.room_number;
     var key = req.body.key;
-    if (!(findRoomByRoomNumber(room_number))) {
-        rooms[room_number] = room_number;
+    if (!(isRoomNumberOccupied(room_number))) {
+        room_numbers[room_number] = room_number;
         if (key) {
             keys[room_number]=key;
         } else {
             if (keys.hasOwnProperty(room_number)) {
                 delete keys[room_number];
             }          
+            public_rooms[room_number] = room_number;
         }
          res.redirect('/' + room_number); 
     } else {
@@ -64,7 +69,7 @@ app.post('/rooms/new', function (req, res) {
 
 function countUsersInRoom(roomName) {
     var room = io.sockets.adapter.rooms[roomName];
-    if (!room) return null;
+    if (!room) return 0;
     return Object.keys(room).length;
 }
 
@@ -95,14 +100,20 @@ io.on('connection', function(socket){
             socket.username = username;
             socket.room = room;
             socket.join(room);
-            
-            if (!(rooms.hasOwnProperty(room))) {
-                rooms[room] = room;
-            }
-        
-            numUsers = countUsersInRoom(room);
             sockets.push(socket);
-        
+            numUsers = countUsersInRoom(room);
+            
+            if (!(room_numbers.hasOwnProperty(room)) && room!='lobby') {
+                room_numbers[room] = room;
+            }
+            if (!(public_rooms.hasOwnProperty(room)) && !(keys.hasOwnProperty(room))) {
+                public_rooms[room]=room;
+                
+            }     
+            if (public_rooms.hasOwnProperty(room)) {
+                numUsersInPublicRooms[room] = numUsers;
+                io.emit('update public rooms', public_rooms, numUsersInPublicRooms);
+            }
             var usernames={};
             for(var i = 0; i < sockets.length; i++) {
                 if (typeof sockets[i] != "undefined" && sockets[i].room==socket.room) {
@@ -143,15 +154,27 @@ io.on('connection', function(socket){
             }
         }
         numUsers = countUsersInRoom(socket.room);
-        if (numUsers==null && socket.room!='lobby') {
-            delete rooms[socket.room];
+        if (numUsers==0 && socket.room!='lobby') {
+            delete room_numbers[socket.room]; // 'cause 'lobby' doesn't exist in rooms_numbers
         } else {
-            if (numUsers!=null) {
+            if (numUsers!=0) {
                 socket.broadcast.to(socket.room).emit('user left', socket.username + ' left');
                 io.to(socket.room).emit('update users', usernames, numUsers);
             }
-        }       
-    });   
+        }  
+        if (public_rooms.hasOwnProperty(socket.room)) {
+            numUsersInPublicRooms[socket.room]=numUsers;
+            if (socket.room!='lobby' && numUsers==0) {
+                delete public_rooms[socket.room];
+                delete numUsersInPublicRooms[socket.room];
+            }
+            io.emit('update public rooms', public_rooms, numUsersInPublicRooms);
+        }
+    });  
+    
+    socket.on('get public rooms', function() {
+        socket.emit('update public rooms', public_rooms, numUsersInPublicRooms);
+    });
 });
 
 var run_server = server.listen(port, function () {
--- static/rooms.js
+++ static/rooms.js
@@ -1,5 +1,7 @@
 var socket = io();
+
 $("body").css("display", "block");
+
 $('#create_room').click(function() {
     if ($('#public').is(':checked')) { 
         document.getElementById("key").required = false;
@@ -14,4 +16,16 @@ $('#private').click(function() {
 
 $('#public').click(function() {
    $("#enter_key").css("display", "none");
+});
+
+socket.on('connect', function() {
+    socket.emit('get public rooms');
+});
+
+socket.on('update public rooms', function(public_rooms, numUsersInPublicRooms) {
+    $('#public_rooms').empty();
+    $.each(public_rooms, function(key, value) {
+        $('#public_rooms').append('<div>'+'<a>'+key+'</a>'+' (' + numUsersInPublicRooms[key] + ')' + '</div>');
+        $("#public_rooms a").last().attr("href",'/'+key);
+    });
 });
